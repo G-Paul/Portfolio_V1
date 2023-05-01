080a3e36e0f75f628b5044215b1716cf1633601e
G-Paul <gunjanpaul99.9@gmail.com>
2023-05-02 02:28:51 +0530

    Stars dalna baki he :)

--- app.js
+++ app.js
@@ -2,29 +2,31 @@ const wrapper = document.getElementById("tiles");
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
-document.querySelector("h1").onmouseover = event => {  
+const doeffect = (e) => {
   let iteration = 0;
   clearInterval(interval);
   
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    e.target.innerText = e.target.innerText
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return e.target.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
+    if(iteration >= e.target.dataset.value.length){ 
       clearInterval(interval);
     }
     
     iteration += 1 / 3;
   }, 30);
 }
+document.querySelector("h1").onmouseover = event => doeffect(event);
+document.querySelector("h1").onclick = event => doeffect(event);
 
 let columns = 0,
 rows = 0,
@@ -72,6 +74,33 @@ const createGrid = () => {
 
   createTiles(columns * rows);
 };
+
+const openLink = link => {
+  window.open(link, "_blank");
+};
+
+// let index = 0;
+// let intervl = 1000;
+
+// const rand = (min, max) => 
+//   Math.floor(Math.random() * (max - min + 1)) + min;
+
+// const animate = star => {
+//   star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+//   star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+//   star.style.animation = "none";
+//   star.offsetHeight;
+//   star.style.animation = "";
+// }
+
+// for(const star of document.getElementsByClassName("magic-star")) {
+//   setTimeout(() => {
+//     animate(star);
+    
+//     setInterval(() => animate(star), 1000);
+//   }, index++ * (intervl / 3))
+// }
 
 createGrid();
 window.onresize = () => createGrid();
